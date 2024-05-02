8f02d8ca83dc497c42c051b62944f89d6fdac13d
Scott <70402467+SWilli93@users.noreply.github.com>
2024-05-01 20:44:21 -0400

    init

---
+++ src/app/page-list/page-list.component.ts
@@ -0,0 +1,18 @@
+import { NgForOf } from '@angular/common';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-page-list',
+  standalone: true,
+  imports: [NgForOf],
+  templateUrl: './page-list.component.html',
+  styleUrl: './page-list.component.css'
+})
+
+export class PageListComponent implements OnInit {
+  @Input() pages = [];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
